4fdad2a49cc7cab20d7c1fe8127c62a11e72c411
Leon van Kammen <leonvankammen@gmail.com>
2025-04-23 16:35:02 +0200

    cat-friendly bundling

--- bundle.sh
+++ bundle.sh
@@ -2,9 +2,12 @@
 OUT=build/aframe-remotestorage
 set -xe
 { 
+  echo "" # leading linebreak for cat-friendly js-bundling
   curl "https://unpkg.com/remotestoragejs@2.0.0-beta.7/release/remotestorage.js" && echo ";" && \
   curl "https://unpkg.com/remotestorage-widget@1.6.0/build/widget.js"            && echo ";" && \
   cat index.js
+  echo "" # trailing linebreak for cat-friendly js-bundling
+
 } > $OUT.js
 
 command -v minify && minify -o $OUT.min.js $OUT.js 
